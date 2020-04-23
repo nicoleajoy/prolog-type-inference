4dd517374ae57d841c43286c3e1f86aab53b633d
Nicole <36486525+nicoleajoy@users.noreply.github.com>
2020-04-23 12:55:09 -0400

    Merged branches and added more test files

--- typeInf/typeInf.pl
+++ typeInf/typeInf.pl
@@ -1,27 +1,32 @@
 :- dynamic gvar/2.
+:- dynamic lvar/2.
 
-typeExp(X, int) :-
-    integer(X).
+% find type of expression
+typeExp(X, int) :- integer(X).
+typeExp(X, float) :- float(X).
+typeExp(X, bool) :- typeBoolExp(X).
 
-typeExp(X, float) :-
-    float(X).
-
-typeExp(X, bool) :-
-    typeBoolExp(X).
-
-/* match functions by unifying with arguments 
+/* match functions by unifying with arguments
     and infering the result
 */
-typeExp(Fct, T):-
+typeExp(Fct, T) :-
     \+ var(Fct), /* make sure Fct is not a variable */ 
     \+ atom(Fct), /* or an atom */
     functor(Fct, Fname, _Nargs), /* ensure we have a functor */
     !, /* if we make it here we do not try anything else */
     Fct =.. [Fname|Args], /* get list of arguments */
-    append(Args, [T], FType), /* make it loook like a function signature */
+    append(Args, [T], FType), /* make it look like a function signature */
     functionType(Fname, TArgs), /* get type of arguments from definition */
     typeExpList(FType, TArgs). /* recurisvely match types */
 
+% local variables
+typeExp(Lvar, T) :-
+    atom(Lvar),
+    lvar(Lvar, T).
+% global variables
+typeExp(Gvar, T) :-
+    atom(Gvar),
+    gvar(Gvar, T).
 
 /* propagate types */
 typeExp(T, T).
@@ -32,57 +37,134 @@ typeExpList([Hin|Tin], [Hout|Tout]):-
     typeExp(Hin, Hout), /* type infer the head */
     typeExpList(Tin, Tout). /* recurse */
 
+% comparison
 hasComparison(int).
 hasComparison(float).
 hasComparison(string).
-
+% add
 hasAdd(int).
 hasAdd(float).
-
+% sub
 hasSub(int).
 hasSub(float).
-
-hasDiv(int).
-hasDiv(float).
-
+% mult
 hasMult(int).
 hasMult(float).
-
-hasEqual(int).
-hasEqual(float).
-
+% div
+hasDiv(int).
+hasDiv(float).
+% equal
+hasEq(int).
+hasEq(float).
+hasEq(string).
+% not equal
+hasNEq(int).
+hasNEq(float).
+hasNEq(string).
+% greater than
 hasGT(int).
 hasGT(float).
-
+% greater than or equal
+hasGTEq(int).
+hasGTEq(float).
+% lesser than
 hasLT(int).
 hasLT(float).
-
-hasNE(int).
-hasNE(float).
+% lesser than or equal
+hasLTEq(int).
+hasLTEq(float).
 
 /* predicate to infer types for boolean expressions */
 typeBoolExp(true).
-typeBoolExp(false). 
-typeBoolExp( X < Y) :- 
+typeBoolExp(false).
+
+typeBoolExp(X = Y) :- 
     typeExp(X, T),
     typeExp(Y, T),
-    hasComparison(T).
+    hasComparison(T),
+    hasEq(X, T),
+    hasEq(Y, T).
+typeBoolExp(X \= Y) :- 
+    typeExp(X, T),
+    typeExp(Y, T),
+    hasComparison(T),
+    hasNEq(X, T),
+    hasNEq(Y, T).
+typeBoolExp(X > Y) :- 
+    typeExp(X, T),
+    typeExp(Y, T),
+    hasComparison(T),
+    hasGT(X, T),
+    hasGT(Y, T).
+typeBoolExp(X < Y) :- 
+    typeExp(X, T),
+    typeExp(Y, T),
+    hasComparison(T),
+    hasLT(X, T),
+    hasLT(Y, T).
 
+/* sum types
+    typeSums :: X -> int 
+    typeSums :: X -> float
+*/
+typeSums(X, Y, T) :-
+    typeSum(Y, T),
+    asserta(gvar(X, T)).
+    % typeExp(X,T),
+    % typeExp(Y,T).
+
+% typeSum(_X, _Y, _T).
+typeSum(X, Y) :-
+    not(X=Y).
+
+/* tuple types
+    typeTuple(tuple(X,Y),(T, R)):-
+*/
+typeTuple(tuple(X, Y, Z), T, R, S):-
+    typeExp(X, T),
+    typeExp(Y, R),
+    typeExp(Z, S),
+    asserta(gvar(X, T)).
 
 /* TODO: add statements types and their type checking */
 
 typeStatement(X, T) :-
     typeExp(X, T).
 
+/* local variable definition
+    Example:
+        lvLet(v, d, T, c) ~ let v = 3 in ...;
+*/
+typeStatement(lvLet(Name, Def, VarT, Code), T):-
+    atom(Name), /* make sure we have a bound name */
+    typeExp(Def, VarT), /* infer the type of var def and ensure it is VarT */
+    bType(VarT), /* make sure we have an inferred type */
+    asserta(lvar(Name, VarT)), /* add definition to database */
+    typeCode(Code, T), % code type
+    retract(lvar(Name, VarT)), % deassert lvar and pop from local database
+    asserta(lvar(Name, VarT) :- false()), % clean wipe
+    bType(T). % make sure that the type of the block is a valid type
+
 /* global variable definition
     Example:
         gvLet(v, T, int) ~ let v = 3;
- */
+*/
 typeStatement(gvLet(Name, T, Code), unit):-
     atom(Name), /* make sure we have a bound name */
     typeExp(Code, T), /* infer the type of Code and ensure it is T */
     bType(T), /* make sure we have an infered type */
     asserta(gvar(Name, T)). /* add definition to database */
+
+/* global function definition
+    Example:
+        let add x y = x + y;
+*/
+typeStatement(gfLet(Name, Args, T, Code), T):-
+    atom(Name), /* make sure we have a bound name */
+    typeExp(Code, T), /* infer the type of Code and ensure it is T */
+    is_list(Args), /* make sure Args is a list */
+    append(Args, [T], X), /* append return type to Args */
+    asserta(gvar(Name, X)). /* add definition to database */
 
 /* if statements are encodes as:
     if(condition:Boolean, trueCode: [Statements], falseCode: [Statements])
@@ -92,6 +174,13 @@ typeStatement(if(Cond, TrueB, FalseB), T) :-
     typeCode(TrueB, T),
     typeCode(FalseB, T).
 
+% for statements Haskell
+typeStatement(where(Name, Cond, Code), T):-
+    typeBoolExp(Cond),
+    atom(Name), /* make sure we have a bound name */
+    typeExp(Code, T), /* infer the type of Code and ensure it is T */
+    asserta(gvar(Code, T)). /* add definition to database */
+
 /* Code is simply a list of statements. The type is 
     the type of the last statement 
 */
@@ -100,15 +189,32 @@ typeCode([S, S2|Code], T):-
     typeStatement(S,_T),
     typeCode([S2|Code], T).
 
+/*
+% code blocks
+typeCode(Code, T),
+    typeExp(X, T).
+    asserta(gvar(Code)),
+    typeExp(Code, (T)), % infer type of Code and ensure it is T 
+*/
+
+/* Code is simply a list of expressions. The type is 
+    the type of the last expression 
+*/
+typeCode([E], T):-typeExpList(E, T).
+typeCode([E, E2|Code], T):-
+    typeExpList(E,_T),
+    typeCode([E2|Code], T).
+
 /* top level function */
 infer(Code, T) :-
     is_list(Code), /* make sure Code is a list */
     deleteGVars(), /* delete all global definitions */
+    deleteLVars(), /* delete all local definitions */
     typeCode(Code, T).
 
 /* Basic types
     TODO: add more types if needed
- */
+*/
 bType(int).
 bType(float).
 bType(string).
@@ -118,7 +224,7 @@ bType(unit). /* unit type for things that are not expressions */
     The type is a list, the last element is the return type
     E.g. add: int->int->int is represented as [int, int, int]
     and can be called as add(1,2)->3
- */
+*/
 bType([H]):- bType(H).
 bType([H|T]):- bType(H), bType(T).
 
@@ -138,7 +244,13 @@ bType([H|T]):- bType(H), bType(T).
     variables. Best wy to do this is in your top predicate
 */
 
-deleteGVars():-retractall(gvar), asserta(gvar(_X,_Y):-false()).
+deleteGVars() :-
+    retractall(gvar), 
+    asserta(gvar(_X,_Y):-false()).
+
+deleteLVars() :-
+    retractall(lvar),
+    asserta(lvar(_X,_Y) :- false()).
 
 /*  builtin functions
     Each definition specifies the name and the 
@@ -147,79 +259,48 @@ deleteGVars():-retractall(gvar), asserta(gvar(_X,_Y):-false()).
     TODO: add more functions
 */
 
-/*
-
-iplus :: int -> int -> int
-
-*/
-
-fType(iplus, [int,int,int]).
-fType((+), [T, T, T]) :- hasAdd(T).
+% convert types
+fType(iToFloat, [int, float]).
+fType(fToInt, [float, int]).
+% add
+fType(iplus, [int, int, int]).
 fType(fplus, [float, float, float]).
-fType(fToInt, [float,int]).
-fType(iToFloat, [int,float]).
-fType(print, [_X, unit]). /* simple print */
-
-% minus
-fType(imin, [int,int,int]).
+fType((+), [T, T, T]) :- hasAdd(T).
+% sub
+fType(iminus, [int, int, int]).
+fType(fminus, [float, float, float]).
 fType((-), [T, T, T]) :- hasSub(T).
-fType(fmin, [float, float, float]).
-fType(fToInt, [float,int]).
-fType(iToFloat, [int,float]).
-fType(print, [_X, unit]). /* simple print */
-
-% division
-fType(idiv, [int,int,int]).
-fType((/), [T, T, T]) :- hasDiv(T).
-fType(fdiv, [float, float, float]).
-fType(fToInt, [float,int]).
-fType(iToFloat, [int,float]).
-fType(print, [_X, unit]). /* simple print */
-
-% multiplication
-fType(imult, [int,int,int]).
-fType((*), [T, T, T]) :- hasMult(T).
+% mult
+fType(imult, [int, int, int]).
 fType(fmult, [float, float, float]).
-fType(fToInt, [float,int]).
-fType(iToFloat, [int,float]).
-fType(print, [_X, unit]). /* simple print */
-
-% Greater Than
-fType(igt, [int,int,int]).
-fType((>), [T, T, T]) :- hasGT(T).
+fType((*), [T, T, T]) :- hasMult(T).
+% div
+fType(idiv, [int, int, int]).
+fType(fdiv, [float, float, float]).
+fType((/), [T, T, T]) :- hasDiv(T).
+% equal
+fType(ieq, [int, int, int]).
+fType(feq, [float, float, float]).
+fType(seq, [string, string, string]).
+fType((==), [T, T, T]) :- hasEq(T).
+% not equal
+fType(ineq, [int, int, int]).
+fType(fneq, [float, float, float]).
+fType(sneq, [string, string, string]).
+fType((/=), [T, T, T]) :- hasNEq(T).
+% greater than
+fType(igt, [int, int, int]).
 fType(fgt, [float, float, float]).
-fType(fToInt, [float,int]).
-fType(iToFloat, [int,float]).
-fType(print, [_X, unit]). /* simple print */
-
-% Less Than
-fType(ilt, [int,int,int]).
-fType((<), [T, T, T]) :- hasLT(T).
+fType((>), [T, T, T]) :- hasGT(T).
+% lesser than
+fType(ilt, [int, int, int]).
 fType(flt, [float, float, float]).
-fType(fToInt, [float,int]).
-fType(iToFloat, [int,float]).
-fType(print, [_X, unit]). /* simple print */
-
-% Equal
-fType(iequal, [int,int,int]).
-fType((==), [T, T, T]) :- hasEqual(T).
-fType(fequal, [float, float, float]).
-fType(fequal, [string, string, string]).
-fType(fToInt, [float,int]).
-fType(iToFloat, [int,float]).
-fType(print, [_X, unit]). /* simple print */
-
-% Not Equal
-fType(inequal, [int,int,int]).
-fType((==), [T, T, T]) :- hasNE(T).
-fType(fnequal, [float, float, float]).
-fType(fnequal, [string, string, string]).
-fType(fToInt, [float,int]).
-fType(iToFloat, [int,float]).
-fType(print, [_X, unit]). /* simple print */
+fType((<), [T, T, T]) :- hasLT(T).
+% simple print
+fType(print, [_X, unit]).
 
 /* Find function signature
-   A function is either buld in using fType or
+   A function is either built in using fType or
    added as a user definition with gvar(fct, List)
 */
 
@@ -228,9 +309,14 @@ functionType(Name, Args):-
     gvar(Name, Args),
     is_list(Args). % make sure we have a function not a simple variable
 
+functionType(Name, Args):-
+    lvar(Name, Args),
+    is_list(Args). % make sure we have a function not a simple variable    
+
 % Check first built in functions
 functionType(Name, Args) :-
     fType(Name, Args), !. % make deterministic
 
 % This gets wiped out but we have it here to make the linter happy
 gvar(_, _) :- false().
+lvar(_, _) :- false().
--- typeInf/typeInf.plt
+++ typeInf/typeInf.plt
@@ -9,23 +9,33 @@
 */
 
 % tests for typeExp
+
 test(typeExp_iplus) :- 
     typeExp(iplus(int,int), int).
 
-% this test should fail
+test(typeExp_iplus_T, [true(T == int)]) :-
+    typeExp(iplus(int, int), T).
+
 test(typeExp_iplus_F, [fail]) :-
     typeExp(iplus(int, int), float).
 
-test(typeExp_iplus_T, [true(T == int)]) :-
-    typeExp(iplus(int, int), T).
+test(typeExp_fplus) :- 
+    typeExp(fplus(float,float), float).
+
+test(typeExp_fplus_T, [true(T == float)]) :-
+    typeExp(fplus(float, float), T).
+
+test(typeExp_fplus_F, [fail]) :-
+    typeExp(fplus(float, float), int).
 
 % NOTE: use nondet as option to test if the test is nondeterministic
 
 % test for statement with state cleaning
 test(typeStatement_gvar, [nondet, true(T == int)]) :- % should succeed with T=int
     deleteGVars(), /* clean up variables */
+    deleteLVars(),
     typeStatement(gvLet(v, T, iplus(X, Y)), unit),
-    assertion(X == int), assertion( Y == int), % make sure the types are int
+    assertion(X==int), assertion(Y==int), % make sure the types are int
     gvar(v, int). % make sure the global variable is defined
 
 % same test as above but with infer 
@@ -37,8 +47,83 @@ test(infer_gvar, [nondet]) :-
 % test custom function with mocked definition
 test(mockedFct, [nondet]) :-
     deleteGVars(), % clean up variables since we cannot use infer
+    deleteLVars(),
     asserta(gvar(my_fct, [int, float])), % add my_fct(int)-> float to the gloval variables
     typeExp(my_fct(X), T), % infer type of expression using or function
     assertion(X==int), assertion(T==float). % make sure the types infered are correct
+
+/* test gvLet (global variables) */
+test(gvLet_i, [nondet]) :- % assign global int variable
+    deleteGVars(),
+    deleteLVars(),
+    typeStatement(gvLet(i, TAssign, iplus(int, int)), T),
+    assertion(T==unit), assertion(TAssign==int).
+
+test(gvLet_f, [nondet]) :- % asssign global float variable
+    deleteGVars(),
+    deleteLVars(),
+    typeStatement(gvLet(i, TAssign, fplus(float, float)), T),
+    assertion(T==unit), assertion(TAssign==float).
+
+/* test lvLet (local variables) */
+test(lvLet_i, [nondet]) :- % assign local int variable
+    deleteGVars(),
+    deleteLVars(),
+    typeStatement(lvLet(v, iplus(int, int), int, [iplus(v,int)]), R),
+    assertion(R==int).
+
+test(lvLet_f, [fail]) :- % assign local float variable
+    deleteGVars(),
+    deleteLVars(),
+    typeStatement(lvLet(v, fplus(X, Y), T, [bool]), unit),
+    assertion(T==float), assertion(X==float), assertion(Y==float),
+    lvar(v, infloatt).
+
+/* test if statement */
+test(if_test, [nondet]) :-
+    typeStatement(if(true, [3], [4]), T),
+    assertion(T==int).
+
+/* test for statement */
+test(where_test, [nondet]) :-
+    typeStatement(where(unit, true, bool), T),
+    assertion(T==bool).
+
+/* infer tests */
+test(infer_test1, [nondet]) :-
+    infer([iplus(int,int), bool], bool).
+
+test(infer_test2, [fail]) :-
+    infer([iplus(int, int)], float).
+
+test(infer_test3, [nondet]) :-
+    infer([fplus(float,float)], float).
+
+test(infer_test4, [fail]) :-
+    infer([fplus(float, float)], int).
+
+test(infer_test5, [nondet]) :-
+    infer([gvLet(u, T1, iplus(_, _)),
+           gvLet(v, T2, fplus(_, _)),
+           iplus(u, int),
+           fplus(v, float)], float),
+    assertion(T1==int),
+    assertion(T2==float).
+
+test(infer_test6, [nondet]) :-
+    infer([gvLet(u, T1, fplus(_, _)),
+           gvLet(v, T2, iplus(_, _)),
+           fplus(u, float),
+           iplus(v, int)], int),
+    assertion(T1==float),
+    assertion(T2==int).
+
+test(infer_test7, [fail]) :-
+    infer([gvLet(u, T1, iplus(_, _)),
+           gvLet(v, T2, fplus(_, _)),
+           fplus(u, int),
+           iplus(v, float)], float),
+    assertion(T1==int),
+    assertion(T2==memory_file_to_atom('Param1', 'Param2')).
 
 :-end_tests(typeInf).
